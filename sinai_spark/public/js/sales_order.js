3a2e8fc1af8e31489c5549cf62272155b68cda70
salmanadayatt <salusalman731@gmail.com>
2025-01-24 16:19:03 +0530

    Revert "feat:connections done."
    This reverts commit d6523c2577bb7aa2b0e19d64326c6e1fc07ec22e.

--- sinai_spark/public/js/sales_order.js
+++
@@ -1,8 +0,0 @@
-frappe.ui.form.on('Sales Order', {
-    refresh: function(frm) {
-        // Remove payment request button
-        setTimeout(() => {
-            frm.remove_custom_button('Payment Request', 'Create');
-        }, 500);
-    }
-});
